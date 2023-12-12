84adf00b32059d253ee03da14e007db235253a7c
kkrajid <rajidkk34@gmail.com>
2023-12-12 10:09:59 +0530

    settings changed asgi

--- hospital_management_/asgi.py
+++ hospital_management_/asgi.py
@@ -1,23 +1,42 @@
 
+# import os
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital_management_.settings')
+
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter , URLRouter
+# from chat.routing import websocket_urlpatterns
+
+
+
+# application = ProtocolTypeRouter(
+#     {
+#         "http" : get_asgi_application() ,
+#         "websocket" : AuthMiddlewareStack(
+#             URLRouter(
+#                 websocket_urlpatterns
+#             )   
+#         )
+#     }
+# )
+
 import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital_management_.settings')
 
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter , URLRouter
-from chat.routing import websocket_urlpatterns
-
-
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat.routing import websocket_urlpatterns  # Move this import here
 
 application = ProtocolTypeRouter(
     {
-        "http" : get_asgi_application() ,
-        "websocket" : AuthMiddlewareStack(
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
             URLRouter(
                 websocket_urlpatterns
-            )   
-        )
+            )
+        ),
     }
 )
-
